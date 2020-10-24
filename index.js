1971d9ad3effaa65632da5543ee26176a6822cbd
Michał Litwin <michaleklitwin@gmail.com>
2020-10-24 19:37:49 +0200

    basic ejs

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.engine(".html", require("ejs").__express);
+
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "html");
+
+app.get("/", (req, res) => {
+    res.render("index", {name: "test", author: "user"});
 });
 
 app.listen(port, () => {
