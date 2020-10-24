8d56d7c369dcc9f998a11999e67cddf8fe4ce38d
Michał Litwin <michaleklitwin@gmail.com>
2020-10-24 20:02:07 +0200

    loading and showing list of tests

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const indexfile = require("./config.json");
+const tests = [];
+for(let testfile of indexfile.files){
+    tests.push(require(testfile));
+}
+
 app.engine(".html", require("ejs").__express);
 
 app.set("views", path.join(__dirname, "views"));
@@ -11,7 +17,7 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("view engine", "html");
 
 app.get("/", (req, res) => {
-    res.render("index", {name: "test", author: "user"});
+    res.render("index", {tests: tests});
 });
 
 app.listen(port, () => {
